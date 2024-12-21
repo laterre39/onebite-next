1209d9a99c59f9aa7d4502685446fe1e9881e0a2
Laterre <laterre39@naver.com>
2024-12-22 01:50:13 +0900

    next 강의 진도 저장 today-241222

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, // 실습을 위해 false <- 리액트의 잠재적 요소를 위해 두번이나 검사를 해서 디버깅에 문제가 생길 수 있음!
 };
 
 export default nextConfig;
